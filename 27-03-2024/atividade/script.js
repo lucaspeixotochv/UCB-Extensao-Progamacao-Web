9b71963a862ea22a473e5fd01cae32a6dfdf0387
Lucas Peixoto Chaves <lucaspeixotochaves@live.com>
2024-03-27 20:49:39 -0300

    atividade 27-03-2024

---
+++ 27-03-2024/atividade/script.js
@@ -0,0 +1,56 @@
+const formulario = document.getElementById("formulario");
+const valorInput = document.getElementById("valor");
+const view = document.getElementById("view");
+class Calculos {
+  listaVendas = [];
+
+  constructor(valor) {}
+
+  adicionar(num) {
+    this.listaVendas.push(Number(num));
+
+    view.innerHTML = `
+      <table>
+        <tr>
+          <td>Maior venda:</td>
+          <td>${this.maiorVenda()}</td>
+        </tr>
+        <tr>
+          <td>Média de vendas:</td>
+          <td>${this.mediaVendas()}</td>
+        </tr>
+        ${this.listaVendas.map((venda) => {
+          return `
+            <tr>
+              <td>Venda:</td>
+              <td>${venda}</td>
+            </tr>
+          `;
+        })}
+      </table>
+    `;
+  }
+
+  maiorVenda() {
+    return Math.max(...this.listaVendas);
+  }
+
+  mediaVendas() {
+    const totalVendas = this.listaVendas.reduce((a, b) => a + b);
+    const media = totalVendas / this.listaVendas.length;
+    return media.toFixed(2);
+  }
+}
+
+const calculos = new Calculos();
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!valorInput.value) {
+    alert("Insira o valor");
+    return;
+  }
+
+  calculos.adicionar(valorInput.value);
+});
